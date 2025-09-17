43c9cb6abb46a5ba770bb3d937758e68bc4ce6e4
Paul O'Neill <pauloneill08@outlook.com>
2025-09-17 17:44:16 +0100

    Scrolling ScrollContainer anr proper .gitignore setup

--- BattleResolver.gd
+++ BattleResolver.gd
@@ -1,17 +1,50 @@
 extends Node
 
-# Takes two arrays of Agent resources and determines a winner.
-func resolve_battle(player_team: Array[Agent], enemy_team: Array[Agent]) -> String:
+func resolve_battle(player_team: Array[Agent], enemy_team: Array[Agent]) -> Array[String]:
+	var combat_log: Array[String] = []
+
+	# Calculate total stats for each team.
 	var player_total_power = 0
+	var player_current_health = 0
 	for agent in player_team:
 		player_total_power += agent.attack_power
+		player_current_health += agent.health
 
-	var enemy_total_health = 0
+	var enemy_total_power = 0
+	var enemy_current_health = 0
 	for agent in enemy_team:
-		enemy_total_health += agent.max_health # Using max_health for this simple calc
+		enemy_total_power += agent.attack_power
+		enemy_current_health += agent.health
+	
+	combat_log.append("Battle Start!")
+	combat_log.append("Player HP: %d | Enemy HP: %d" % [player_current_health, enemy_current_health])
+	combat_log.append("--------------------")
 
-	# If the player's total power is greater than the enemy's total health, they win.
-	if player_total_power > enemy_total_health:
-		return "Player Team Wins!"
+	# 2. The Battle Loop.
+	var turn = 1
+	while player_current_health > 0 and enemy_current_health > 0:
+		combat_log.append("Turn %d:" % turn)
+		
+		# Player attacks enemy.
+		enemy_current_health -= player_total_power
+		combat_log.append("-> Player team attacks for %d damage!" % player_total_power)
+		
+		# Check if the enemy team is defeated.
+		if enemy_current_health <= 0:
+			break
+			
+		# Enemy attacks player.
+		player_current_health -= enemy_total_power
+		combat_log.append("-> Enemy team attacks for %d damage!" % enemy_total_power)
+		
+		combat_log.append("Player HP: %d | Enemy HP: %d" % [player_current_health, enemy_current_health])
+		combat_log.append("--------------------")
+		turn += 1
+
+	# 3. Determine the winner and add it to the log.
+	if player_current_health > 0:
+		combat_log.append("PLAYER TEAM WINS!")
 	else:
-		return "Enemy Team Wins!"
+		combat_log.append("ENEMY TEAM WINS!")
+		
+	return combat_log
--- test_scene.gd
+++ test_scene.gd
@@ -1,6 +1,10 @@
 extends Node
 
-@onready var label = $Label
+@onready var scroll_container = $ScrollContainer
+@onready var label = $ScrollContainer/Label
+
+func _ready():
+	label.resized.connect(_scroll_to_bottom)
 
 func _on_button_pressed():
 	# 1. Create the teams using our factory.
@@ -13,7 +17,11 @@ func _on_button_pressed():
 		enemy_team.append(RosterManager.generate_agent())
 
 	# 2. Pass the teams to our new resolver.
-	var result_string = BattleResolver.resolve_battle(player_team, enemy_team)
+	var combat_log_array = BattleResolver.resolve_battle(player_team, enemy_team)
 
 	# 3. Display the result.
-	label.text = result_string
+	label.text = "\n".join(combat_log_array)
+
+func _scroll_to_bottom():
+	await get_tree().process_frame
+	scroll_container.scroll_vertical = scroll_container.get_v_scroll_bar().max_value
